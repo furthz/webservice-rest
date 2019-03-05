5ecd81fb551401e43769ff96cd8011498051d0cc
NelidaMaribi <nelida.manosalva@soapros.pe>
2019-03-05 14:50:48 -0500

    nuevo Bean

---
+++ src/main/java/pe/soapros/generacionccm/beans/DetalleRespuesta.java
@@ -0,0 +1,48 @@
+package pe.soapros.generacionccm.beans;
+
+public class DetalleRespuesta {
+	String numero;
+	String archivo;
+	String indExito;
+	String codEstado;
+	String msgEstado;
+	String valorRetorno;
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getArchivo() {
+		return archivo;
+	}
+	public void setArchivo(String archivo) {
+		this.archivo = archivo;
+	}
+	public String getIndExito() {
+		return indExito;
+	}
+	public void setIndExito(String indExito) {
+		this.indExito = indExito;
+	}
+	public String getCodEstado() {
+		return codEstado;
+	}
+	public void setCodEstado(String codEstado) {
+		this.codEstado = codEstado;
+	}
+	public String getMsgEstado() {
+		return msgEstado;
+	}
+	public void setMsgEstado(String msgEstado) {
+		this.msgEstado = msgEstado;
+	}
+	public String getValorRetorno() {
+		return valorRetorno;
+	}
+	public void setValorRetorno(String valorRetorno) {
+		this.valorRetorno = valorRetorno;
+	}
+	
+	
+}
